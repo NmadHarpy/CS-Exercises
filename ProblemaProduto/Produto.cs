f7a930ed31f9081177c2e0ad13817048cd256aae
Raphael Galera <raphagalera.r@gmail.com>
2021-11-10 00:49:51 -0300

    add products-exercise

---
+++ ProblemaProduto/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace ProblemaProduto
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double valorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade + " unidades, Total: $ " 
+                + valorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
