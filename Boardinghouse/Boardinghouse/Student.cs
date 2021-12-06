9af0d0cc95a55eb29b9b5e225736586063c1829d
Raphael Galera <raphagalera.r@gmail.com>
2021-12-05 22:50:37 -0300

    boardinghouse exercise added

---
+++ Boardinghouse/Boardinghouse/Student.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Boardinghouse
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Student (string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + " ," + Email;
+        }
+    }
+}
