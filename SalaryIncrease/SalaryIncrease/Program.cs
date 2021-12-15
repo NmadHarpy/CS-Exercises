b544c2deb6654fb65a746ad23d04483f0067db11
Raphael Galera <raphagalera.r@gmail.com>
2021-12-15 16:19:39 -0300

    employee salary increase

---
+++ SalaryIncrease/SalaryIncrease/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SalaryIncrease
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" +i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine("\n");
+            }
+
+            Console.Write("Enter the employee ID that will have a salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+            if(emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This ID does not exist.");
+            }
+
+            Console.WriteLine("Updated list of employees:");
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
